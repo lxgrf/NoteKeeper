697d800c204206001af26c7b67c24c6943a1a53c
Alex Griffiths <github@lxgrf.co.uk>
2024-09-08 22:32:03 +0100

    Initial Commit

---
+++ src/discord/__init__.py
@@ -0,0 +1,3 @@
+from .discord_bot import bot, tree, guild_check
+
+__all__ = ['bot', 'tree', 'guild_check']
---
+++ src/notion/__init__.py
@@ -0,0 +1,3 @@
+from .notion_extract import extract_notion_docs, create_embeddings
+
+__all__ = ['extract_notion_docs', 'create_embeddings']
