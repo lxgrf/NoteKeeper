ff6b09a8de248d1ec07b2d40eef388c372502256
Alex Griffiths <github@lxgrf.co.uk>
2024-09-11 14:53:08 +0100

    expanded to new functions

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,3 +1,15 @@
-from .database import get_existing_ids, store_embeddings
+from .database import (
+    get_chroma_client,
+    get_or_create_chroma_collection,
+    get_existing_ids_chroma,
+    store_embeddings_chroma,
+    process_and_store_embeddings_chroma
+)
 
-__all__ = ['get_existing_ids', 'store_embeddings']
+__all__ = [
+    "get_chroma_client",
+    "get_or_create_chroma_collection",
+    "get_existing_ids_chroma",
+    "store_embeddings_chroma",
+    "process_and_store_embeddings_chroma"
+]
